b5d45e6eb4d8c8cb1fc4ccb1ec85d25c3214cc83
flying_high <568115405@qq.com>
2019-04-04 20:20:05 +0800

    首页展示，网站图标设置

--- infor/modules/index/views.py
+++ infor/modules/index/views.py
@@ -1,7 +1,13 @@
 from . import index_blu
-from flask import current_app
+from flask import render_template, current_app
 
 
 @index_blu.route('/')
 def index():
-    return 'index'
+    return render_template('news/index.html')
+
+
+@index_blu.route('/favicon.ico')
+def web_log():
+
+    return current_app.send_static_file('news/favicon.ico')
